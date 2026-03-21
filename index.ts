cef70f363d5ff30c044e3afd9e50a1fab82f397a
Hireophant <tntdanh2506@gmail.com>
2026-03-21 15:35:25 +0700

    static file

--- index.ts
+++ index.ts
@@ -11,6 +11,9 @@ import clientRoutes from "./routes/client/index.route";
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
+
+app.use(express.static("public"));
+
 app.set("views", "./views")
 app.set("view engine", "pug");
 
