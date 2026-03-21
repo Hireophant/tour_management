22004cc9153aeae597c9288e576ba1c918677ae0
Hireophant <tntdanh2506@gmail.com>
2026-03-21 15:32:43 +0700

    restruct folder pug

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -7,6 +7,7 @@ export const index = async (req: Request, res: Response) => {
     // SELECT * FROM tours WHERE deleted = false AND status = "active";
     const tours = await Tour.findAll({ raw: true, where: { deleted: false, status: "active" } });
     res.render("client/pages/tours/index", {
+        pageTitle: "Danh sách tour",
         tours: tours
     });
 }
